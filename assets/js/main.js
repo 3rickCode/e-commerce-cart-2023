ff7ab64c3b7ea1ff3017bfddf098f5d72a3b2ee1
Erick Code <3rickcedeno@gmail.com>
2023-05-06 19:09:39 -0500

    Creating JS Interaction

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+import loader from './components/loader.js';   
+import showMenu from './components/showMenu.js';
+import showCart from './components/showCart.js';
+import products from './components/products.js';
+import getProducts from './helpers/getProducts.js';
+import cart from './components/cart.js';
+
+/* Ocultar loader */
+loader()
+
+/* Mostrar menu */
+showMenu()
+
+
+/* Mostrar cart */
+showCart()
+
+/* End UI Elements */
+
+/* productos  */
+const {db, printProducts} =products(await getProducts())
+
+/* carrito */
+cart(db,printProducts)
