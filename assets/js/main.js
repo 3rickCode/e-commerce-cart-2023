1ebca42d5ea8738057d7f5eae901ea745fc3e43e
Fabian Ramos <fabian2103@gmail.com>
2023-05-10 01:53:30 -0500

    add dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
+import theme from './components/theme.js';
 import loader from './components/loader.js';   
 import showMenu from './components/showMenu.js';
 import showCart from './components/showCart.js';
 import products from './components/products.js';
 import getProducts from './helpers/getProducts.js';
 import cart from './components/cart.js';
+
+/* Theme */
+theme()
 
 /* Ocultar loader */
 loader()
