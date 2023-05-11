b1d158bc53c12781ef67810c6a9f3bb11c9968c6
Kevin Cifuentes <kevmusic25@gmail.com>
2023-05-11 16:43:37 -0500

    Dark Mode functional

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
-import loader from './components/loader.js';   
-import showMenu from './components/showMenu.js';
-import showCart from './components/showCart.js';
-import products from './components/products.js';
-import getProducts from './helpers/getProducts.js';
-import cart from './components/cart.js';
+import loader from './components/loader.js'
+import showMenu from './components/showMenu.js'
+import showCart from './components/showCart.js'
+import products from './components/products.js'
+import getProducts from './helpers/getProducts.js'
+import cart from './components/cart.js'
 import showDescription from './components/showDescription.js'
-import dataBase from './helpers/getProducts.js';
+import dataBase from './helpers/getProducts.js'
+import theme from './components/theme.js'
 
 /* Ocultar loader */
 loader()
@@ -13,17 +14,19 @@ loader()
 /* Mostrar menu */
 showMenu()
 
-
 /* Mostrar cart */
 showCart()
 
 /* End UI Elements */
 
 /* productos  */
-const {db, printProducts} =products(dataBase)
+const { db, printProducts } = products(dataBase)
 
 /* carrito */
-cart(db,printProducts)
+cart(db, printProducts)
 
 /*Mostrar descripción */
 showDescription()
+
+/* Dark Mode */
+theme()
